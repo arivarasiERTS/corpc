e7fdb5e1161dfb2e55a73ab73d9fd28045e92d0f
ArivarasiERTS <ArivarasiERTS>
2018-02-15 04:42:27 +0530

    added pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
 @Component({
   templateUrl: 'app.html'
 })
@@ -19,10 +20,11 @@ export class MyApp {
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
+
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Login', component: HomePage },
-      { title: 'Home', component: ListPage }
+      { title: 'Login', component: LoginPage},
+      { title: 'Home', component: HomePage}
     ];
 
   }
